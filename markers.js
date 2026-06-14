5743d379c3570ef1dfa2121111ff5c1476d89164
blackfuga <larrygenus6@gmail.comgit pushcd ~/Desktop/mymapgit initgit add .git commit -m first>
2026-06-13 23:53:25 -0500

    add server markers

--- markers.js
+++ markers.js
@@ -5,6 +5,36 @@ const MARKERS = [
     z: 0,
     desc: "World spawn"
   },
+    {
+    name: "End portal",
+    x: 1500,
+    z: -50,
+    desc: "spawn end portal"
+  },
+      {
+    name: "Blackfuga's Base",
+    x: -100,
+    z: -750,
+    desc: "blackfugas village/base/farm"
+  },
+        {
+    name: "Kirkistan",
+    x: -160,
+    z: -420,
+    desc: "kirkistan obsidian city"
+  },
+          {
+    name: "Themaxstaff base",
+    x: 0,
+    z: -1337,
+    desc: "themaxstaff's base"
+  },
+            {
+    name: "Myrmeciaville",
+    x: -2050,
+    z: -2050,
+    desc: "myrmecia build"
+  },
 
   // Add more markers below by copying the block above
   // {
