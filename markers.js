7b8241f4ea7b457688130c0c15ae32746a9f6dcd
blackfuga <larrygenus6@gmail.comgit pushcd ~/Desktop/mymapgit initgit add .git commit -m first>
2026-06-13 23:46:31 -0500

    remove emojis, add marker toggle

--- markers.js
+++ markers.js
@@ -3,7 +3,15 @@ const MARKERS = [
     name: "Spawn",
     x: 0,
     z: 0,
-    icon: "🌍",
     desc: "World spawn"
-  }
+  },
+
+  // Add more markers below by copying the block above
+  // {
+  //   name: "My Base",
+  //   x: 1000,
+  //   z: -500,
+  //   desc: "Main base"
+  // },
+
 ];
